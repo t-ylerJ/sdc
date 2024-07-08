c4b4c956316231da212cb888e62fbd862dfebfa4
Tyler Johnson <tylerj0937@gmail.com>
2024-07-07 21:00:02 -0700

    Copy k6 test to artillery

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
 import http from 'k6/http';
-// import { sleep } from 'k6';
+import { sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '2m', target: 100 },
-    { duration: '4m', target: 100},
-    { duration: '2m', target: 0}
+    { duration: '4m', target: 100 },
+    { duration: '6m', target: 200},
+    { duration: '4m', target: 0}
   ]
-
-
-  // The following section contains configuration options for execution of this
-  // test script in Grafana Cloud
-  //
-  // See https://grafana.com/docs/grafana-cloud/k6/get-started/run-cloud-tests-from-the-cli/
-  // to learn about authoring and running k6 test scripts in Grafana k6 Cloud.
-  //
-  // cloud: {
-  //   // The ID of the project to which the test is assigned in the k6 Cloud UI.
-  //   // By default tests are executed in default project.
-  //   projectID: "",
-  //   // The name of the test in the k6 Cloud UI.
-  //   // Test runs with the same name will be grouped.
-  //   name: "script.js"
-  // },
-
-
 };
 
 
