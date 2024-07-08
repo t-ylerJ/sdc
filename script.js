2518d713cb92ef1601b8394d0b9da50beabfc682
Tyler Johnson <tylerj0937@gmail.com>
2024-07-07 20:24:14 -0700

    k6 tests at 500 throughput

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+// import { sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '1m', target: 10 },
-    { duration: '3m', target: 10},
-    { duration: '1m', target: 0}
+    { duration: '2m', target: 100 },
+    { duration: '4m', target: 100},
+    { duration: '2m', target: 0}
   ]
 
 
@@ -24,34 +24,10 @@ export const options = {
   //   name: "script.js"
   // },
 
-  // Uncomment this section to enable the use of Browser API in your tests.
-  //
-  // See https://grafana.com/docs/k6/latest/using-k6-browser/running-browser-tests/ to learn more
-  // about using Browser API in your test scripts.
-  //
-  // scenarios: {
-  //   // The scenario name appears in the result summary, tags, and so on.
-  //   // You can give the scenario any name, as long as each name in the script is unique.
-  //   ui: {
-  //     // Executor is a mandatory parameter for browser-based tests.
-  //     // Shared iterations in this case tells k6 to reuse VUs to execute iterations.
-  //     //
-  //     // See https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/ for other executor types.
-  //     executor: 'shared-iterations',
-  //     options: {
-  //       browser: {
-  //         // This is a mandatory parameter that instructs k6 to launch and
-  //         // connect to a chromium-based browser, and use it to run UI-based
-  //         // tests.
-  //         type: 'chromium',
-  //       },
-  //     },
-  //   },
-  // }
+
 };
 
 
 export default function() {
   http.get('https://test.k6.io');
-  sleep(.1);
 }
--- server/index.js
+++ server/index.js
@@ -12,18 +12,6 @@ app.use(morgan('dev'));
 app.use(express.static(path.join('/Users/tylerjohnson/hackreactor/rfp2404/arrow_sdc/questions/client/public')));
 
 
-// router.get('/questions', getQuestions);
-// router.post('/questions', postQuestion);
-// router.put('questions/:question_id/helpful', markQuestionHelpful);
-// router.put('questions/:question_id/report', reportQuestion);
-
-// router.get('questions/:question_id/answers', getAnswers);
-// router.post('questions/:question_id/answers', postAnswer);
-// router.put('/answers/:answer_id/helpful', markAnswerHelpful);
-// router.put('/answers/:answer_id/report', reportAnswer);
-
-// app.use('/qa', router);
-
 router.get('/answers', getAnswers);
 router.post('/answers', postAnswer);
 router.put('/helpful', markQuestionHelpful);
@@ -45,15 +33,6 @@ app.listen(PORT, () => {
 });
 
 
-//Example requests to assist with testing:
-//getQuestions:
-//http://localhost:3000/qa/questions?product_id=998998 //update product_id in query
-//getAnswer:
-//postAnswer:
-//http://localhost:3000/qa/questions/:question_id/answers?question_id=1
-// {
-//   "body": "Est eum rerum mollitia inventore veniam.",
-//   "answerer_name": "Jayce.Romaguera17",
-//   "answerer_email": "first.last@gmail.com",
-//   "photos": ["https://images.unsplash.com/photo-1553830591-2f39e38a013c?ixlib=rb-1.2.1&auto=format&fit=crop&w=2760&q=80", "https://images.unsplash.com/photo-1522032238811-74bc59578599?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"]
-// }
+
+
+
